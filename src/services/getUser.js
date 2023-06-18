6704241255be35f9db1f3d83226d3c7ec0ba71ac
Vicentebp <vicentebpberwanger@gmail.com>
2023-06-18 15:50:39 -0300

    Funcionalidades de login e register

---
+++ src/services/getUser.js
@@ -0,0 +1,13 @@
+const getUser = (email) => {
+  const userEmail = email ?? localStorage.getItem("auth");
+  if (localStorage.getItem("users") == null) localStorage.setItem("users", "[]");
+  const arrUsers = JSON.parse(localStorage.getItem("users"));
+  arrUsers.find((item) => {
+    if (item.email === userEmail) {
+      return userEmail;
+    }
+  });
+  throw "Email not registred";
+};
+export default getUser;
+b;
