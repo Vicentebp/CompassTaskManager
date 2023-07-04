d562515b885bba5dec0e75ace6bfb1e52a05c1ec
Vicentebp <vicentebpberwanger@gmail.com>
2023-07-04 20:18:24 -0300

    Teste1

--- src/services/createUser.js
+++ src/services/createUser.js
@@ -1,9 +1,10 @@
 const createUser = (newUser) => {
-  if (localStorage.getItem("users") == null) localStorage.setItem("users", "[]");
+  // if (localStorage.getItem("users") == null) localStorage.setItem("users", "[]");
 
-  const arrUsers = JSON.parse(localStorage.getItem("users"));
-  if (arrUsers.find((item) => item.email === newUser.email)) throw "Email already used";
-  arrUsers.push(newUser);
-  localStorage.setItem("users", JSON.stringify(arrUsers));
+  // const arrUsers = JSON.parse(localStorage.getItem("users"));
+  // if (arrUsers.find((item) => item.email === newUser.email)) throw "Email already used";
+  // arrUsers.push(newUser);
+  // localStorage.setItem("users", JSON.stringify(arrUsers));
+  return 5;
 };
 export default createUser;
